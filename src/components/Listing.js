96ef4feffb5004498f8bc0a3aa1c04f50dfbddf7
Noreen Wu <noreenwu@gmail.com>
2019-10-24 17:53:25 -0400

    feat: added links to the listing, if they aren't null

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -4,11 +4,30 @@ import PropTypes from 'prop-types'
 function Listing (props) {
   const { listItem } = props
 
+  let links = []
+  // assemble an array of non-null links to present
+  if ( listItem.homepage_uri !== null ) {
+     links.push({what: 'homepage_uri', url: listItem.homepage_uri})
+  }
+  if ( listItem.gem_uri !== null ) {
+    links.push({what: 'gem_uri', url: listItem.gem_uri})
+  }
+  if ( listItem.wiki_uri !== null ) {
+    links.push({what: 'wiki_uri', url: listItem.wiki_uri})
+  }
+  if ( listItem.source_code_uri !== null ) {
+    links.push({what: 'source_code_uri', url: listItem.source_code_uri})
+  }
+
   return (
     <Fragment>
       <li key={listItem.sha}><div><span className="strong">{listItem.name}</span> by {listItem.authors} </div>
         <span className="description">{listItem.info}</span>
-
+          <ul className="links">
+          { links.map((uri) => (
+              <li key={uri.what}>{uri.what} {uri.url}</li>
+          ))}
+          </ul>
       </li>
       <button className={`btn`}>Save</button>
     </Fragment>
