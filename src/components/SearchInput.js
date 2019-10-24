15aa77cf40d8c94da8db069a68d7e0c1ba212636
Noreen Wu <noreenwu@gmail.com>
2019-10-24 09:29:38 -0400

    feat: decided to search as the user types instead of as a result of clicking submit. results return; need to be limited

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -6,15 +6,53 @@ class SearchInput extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      query: ''
+      query: '',
+      searchResult: ['a', 'b', 'c']
     }
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(e) {
+      console.log("handleChange")
       this.setState({
         [e.target.name]: e.target.value
       })
+
+      let query = e.target.value
+
+      if (query !== '') {
+        get(query)
+          .then(console.log("got results"))
+          .catch((err) => {
+             console.log("search error", err)
+          })
+      }
+      // this.setState ({
+      //   searchResult : ['a', 'b']
+      // })
+  }
+
+  updateQuery(query) {
+      this.setState(() => ({
+        query: query
+      }))
+
+      if (query === '') {
+        this.setState({
+          searchResult : [],
+        })
+      }
+      else {
+        get(query.toLowerCase())
+          .then((res) => {
+             let newResults = res
+             console.log("res", res, res.length)
+             // this.setState({ searchResult: newResults })
+          })
+          .catch((err) => {
+             console.log("search error", err)
+          })
+      }
   }
 
   handleSubmit(e) {
@@ -27,15 +65,16 @@ class SearchInput extends Component {
   }
 
   render() {
+    console.log("query is ", this.state.query)
     return(
       <div>
         <form className='search-box' onSubmit={this.handleSubmit}>
         <input
             name="query"
             type="text"
-            placeholder="your search terms"
+            placeholder="Search for gems by keyword"
             value={this.state.query}
-            onChange={this.handleChange}
+            onChange={(event) => this.updateQuery(event.target.value)}
             className='text-area'
             maxLength={100}
             size={80}
@@ -61,7 +100,19 @@ class SearchInput extends Component {
            >
            View Saved
         </button>
+
         </form>
+
+        <div className="search-results">
+                <h3>hello</h3>
+                <div>{this.state.searchResult[0]}</div>
+                { this.state.searchResult.length === 0
+                  ? <li key='1'>no result</li>
+                  : this.state.searchResult.map((id) => (
+                      <li key={id}>something</li>
+                  ))
+                }
+        </div>
       </div>
     )
   }
